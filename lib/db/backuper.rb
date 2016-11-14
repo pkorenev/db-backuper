9e0c244fef5603ed334e6d8b1a5f39cfd810ec0f
pkorenev <p.korenev@voroninstudio.eu>
2016-11-14 21:27:25 +0200

    fix

--- lib/db/backuper.rb
+++ lib/db/backuper.rb
@@ -1,4 +1,5 @@
-Dir[File.expand_path("../tasks/**/*.rake", __FILE__)].each { |ext| require ext } #if defined?(Rake)
+#Dir[File.expand_path("../tasks/**/*.rake", __FILE__)].each { |ext| require ext } #if defined?(Rake)
+
 require "db/backuper/version"
 require 'rails/railtie'
 
@@ -12,6 +13,7 @@ module Db
           load ext
         end
 
+        load File.expand_path("../tasks/db.rake", __FILE__)
       end
     end
   end
