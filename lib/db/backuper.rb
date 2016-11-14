dc4f6949beccc729d2b6a079c9df0a4061f7c5d8
pkorenev <p.korenev@voroninstudio.eu>
2016-11-14 21:16:37 +0200

    test

--- lib/db/backuper.rb
+++ lib/db/backuper.rb
@@ -1,8 +1,18 @@
 Dir[File.expand_path("../tasks/**/*.rake")].each { |ext| require ext } #if defined?(Rake)
 require "db/backuper/version"
+require 'rails/railtie'
 
 module Db
   module Backuper
     # Your code goes here...
+
+    class Railtie < Rails::Railtie
+      rake_tasks do
+        Dir[File.expand_path("../tasks/**/*.rake", __FILE__)].each do |ext|
+          load ext
+        end
+
+      end
+    end
   end
 end
